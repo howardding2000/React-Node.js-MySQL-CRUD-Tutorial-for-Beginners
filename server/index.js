2b579aba438b2972427c2819e48ad1ccf381e26f
howard <howardding2000@gmail.com>
2022-11-22 22:06:35 -0500

    complate client code

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mysql from "mysql";
+import cors from "cors";
 
 const app = express();
 
@@ -15,12 +16,15 @@ const db = mysql.createConnection({
 
 app.use(express.json());
 
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.json("hello this is the server");
 });
 
 app.get("/books", (req, res) => {
   const q = "SELECT * FROM books";
+
   db.query(q, (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
@@ -28,16 +32,48 @@ app.get("/books", (req, res) => {
 });
 
 app.post("/books", (req, res) => {
-  const q = "INSERT INTO books (`title`, `desc`,`cover`) VALUES (?)";
   const values = [
-   req.body.title,
-   req.body.desc,
-   req.body.cover,
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
   ];
+
+  const q = "INSERT INTO books (`title`, `desc`,`price`,`cover`) VALUES (?)";
 
   db.query(q, [values], (err, data) => {
     if (err) return res.json(err);
     return res.json("Book has been created successfully.");
+  });
+});
+
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+
+  const q = "DELETE FROM books WHERE id = ?";
+
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been deleted successfully.");
+  });
+});
+
+app.put("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  const q =
+    "UPDATE books SET `title` = ?, `desc` = ?, `price` = ?, `cover` = ? WHERE `id` = ? ";
+
+  db.query(q, [...values, bookId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been updated successfully.");
   });
 });
 
